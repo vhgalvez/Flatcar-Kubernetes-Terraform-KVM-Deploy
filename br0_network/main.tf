585d9e4f5dd3ede8af6ec9126a2c7a26de26a11b
Victor Galvez <vhgalvez@gmail.com>
2024-11-19 17:38:50 +0100

    Update main.tf

--- br0_network/main.tf
+++ br0_network/main.tf
@@ -1,11 +1,11 @@
 # br0_network\main.tf
 terraform {
-  required_version = "= 1.9.6"
+  required_version = "= 1.9.8"
 
   required_providers {
     libvirt = {
       source  = "dmacvicar/libvirt"
-      version = "0.8.0"
+      version = "0.8.1"
     }
   }
 }
